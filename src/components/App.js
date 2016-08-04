931842c55da369118fed89fbd483263b6836e31b
LimaGuilherme <guilherme.lb@outlook.com>
2016-08-03 21:05:48 -0300

    project finish

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,25 @@
  * Created by Guilherme Lima on 03/08/2016.
  */
 import React from 'react';
-import Contador from './Contador';
+import PlacarContainer from './PlacarContainer';
+
+const dados = {
+    partida: {
+        estadio: "Allianz Parque",
+        data:  "03/08/2016",
+        horario: "23h",
+    },
+    casa: {
+        nome: "Palmeiras"
+    },
+    visitante:{
+        nome: "Santos"
+    },
+}
 
 export default class App extends React.Component{
     render(){
-        return <Contador/>
+        return <PlacarContainer {...dados} clima={'Ensolarado'} tempo={92}/>
 
     }
 
